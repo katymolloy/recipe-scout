d970c5d5d90a28a43831b704975ceb90d42b16c0
Matthew Gooding <goodmatt190@gmail.com>
2024-04-03 15:34:15 -0400

    404 Page added and styled

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
+import Header from "../../Components/Header"
+import Footer from "../../Components/Footer"
+
+import "./NotFound.scss";
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>404: Not Found</h1>
-            <div>Oops, looks like you may be lost.</div>
-            <Link to={'/'}>Take me back home!</Link>
+        <div className="notFound">
+            <Header />
+            <main>
+                <h1>404: Not Found</h1>
+                <div>Oops, looks like you may be lost.</div>
+                <Link to={'/'}>Take me back home!</Link>
+            </main>
+            <Footer />
         </div>
     )
 }
